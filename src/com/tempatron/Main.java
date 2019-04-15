91b4df9cd54c6828184001841c7a372218f8b72b
Abhinav <abhinavraut420@gmail.com>
2019-04-16 00:23:26 +0530

    Added selection sort

--- src/com/tempatron/Main.java
+++ src/com/tempatron/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //bubble sort // Quadratic complexity n^2
-        int[] num = {23,10,99,01,55,78,-22};
+        int[] num = { 64, 34, 25, 12, 22, 11, 90 };
 
         for(int lastUnsortedIndex = num.length - 1 ; lastUnsortedIndex > 0 ; lastUnsortedIndex --  ){
             for(int i = 0 ; i < lastUnsortedIndex ; i++){
@@ -15,18 +15,37 @@ public class Main {
             }
         }
 
-        for(int i = 0 ; i < num.length ; i++){
-            System.out.println(num[i]);
+        printArray(num);
+
+        int[] num1 = { 64, 34, 25, 12, 22, 11, 90 };
+
+        for(int lastUnsortedIndex = num1.length - 1 ; lastUnsortedIndex > 0 ; lastUnsortedIndex --) {
+            int largest = 0;
+            for(int i = 1 ; i <= lastUnsortedIndex ; i++ ){
+                if(num1[i] > num1[largest]){
+                    largest = i;
+                }
+            }
+            swap(num1,lastUnsortedIndex,largest);
         }
+
+        printArray(num1);
     }
 
-    public static void swap(int[] array,int i , int j){
+  private static void swap(int[] array,int i , int j){
         if(i == j) {
-            return;
+
         }else{
             int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }
